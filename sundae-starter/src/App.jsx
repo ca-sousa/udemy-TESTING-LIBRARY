48852606b78a9934e5fd475db897a36b7e10d89d
Carla Sousa <sousa.carla09@gmail.com>
2024-03-18 09:15:58 -0300

    feat: scoop cal for subtotal test

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,8 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+import OrderEntry from "./pages/entry/OrderEntry";
+
 function App() {
   return (
-    <div>
-      <h1>Sundaes on Demand</h1>
-    </div>
+  <Container>
+    <OrderDetailsProvider>
+      <OrderEntry />
+    </OrderDetailsProvider>
+  </Container>
   );
 }
 
