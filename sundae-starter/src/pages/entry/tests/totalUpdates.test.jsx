00be3e0cbbc82fdb0fb27c9206f724db8961f85a
Carla Sousa <sousa.carla09@gmail.com>
2024-03-18 09:29:18 -0300

    feat: pass context when rendering the page

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 import userEvent from "@testing-library/user-event";
+import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-  render(<Options optionType="scoops" />);
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider});
 
   // make sure total starts out $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
